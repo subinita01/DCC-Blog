4447c41b3455593b0b88a15538f65f17714c5205
subinita01 <subinitaray07@gmail.com>
2024-11-19 11:34:03 +0530

    Changes

---
+++ data/projectsData.ts
@@ -0,0 +1,23 @@
+interface Project {
+  title: string,
+  description: string,
+  href?: string,
+  imgSrc?: string,
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Slot Available',
+    description: `This slot is open for sponsors! If you are interested in sponsoring us, please contact us at dccnita@gmail.com. We accept sponsorships only from brands that we trust ourselves.`,
+    imgSrc: '/static/images/ninjas.jpeg',
+    href: 'mailto:dccnita@gmail.com',
+  },
+  {
+    title: 'Slot Available ',
+    description: `This slot is open for sponsors! If you are interested in sponsoring us, please contact us at dccnita@gmail.com. We accept sponsorships only from brands that we trust ourselves.`,
+    imgSrc: '/static/images/ninjas.jpeg',
+    href: 'mailto:dccnita@gmail.com',
+  },
+]
+
+export default projectsData
---
+++ next.config.js
@@ -0,0 +1,90 @@
+const { withContentlayer } = require('next-contentlayer2');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app analytics.umami.is us.umami.is;
+  style-src 'self' 'unsafe-inline';
+  img-src * blob: data:;
+  media-src *.s3.amazonaws.com;
+  connect-src *;
+  font-src 'self';
+  frame-src giscus.app
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+module.exports = () => {
+  const plugins = [withContentlayer, withBundleAnalyzer];
+  return plugins.reduce((acc, next) => next(acc), {
+    reactStrictMode: true,
+    pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+    eslint: {
+      dirs: ['app', 'components', 'layouts', 'scripts'],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+          pathname: '**',
+        },
+        {
+          protocol: 'http',
+          hostname: '**',
+          pathname: '**',
+        },
+      ],
+    },
+    async headers() {
+      return [
+        {
+          source: '/(.*)',
+          headers: securityHeaders,
+        },
+      ];
+    },
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+
+      return config;
+    },
+  });
+};
